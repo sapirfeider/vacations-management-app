613be776dbf7894978526a4d07a69a4018b3ef24
Sapir95 <sapirfeider95@gmail.com>
2021-07-29 14:53:34 +0300

    debug___

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const api = express();
 // api.use(cors());
 const path = require('path');
 const getConnection = require("./database/connectDB")
-const { getVacations } = require("./controllers/actionsByUser")
+const { getVacations , getCategories} = require("./controllers/actionsByUser")
 
 global.__basedir = __dirname;
 
@@ -19,7 +19,8 @@ api.use(async (req, res, next) => {
     await initConnection();
     if (global.connection) {
         try {
-            const vacations = await getVacations(2)
+            //const vacations = await getVacations(2)
+            const vacations = await getCategories()
             res.status(200).send(vacations)
         } catch (ex) {
             console.log(ex)
